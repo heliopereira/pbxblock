eda5ddde582e5b6fea2067a4dc9d646653af95f2
zombu2 <zombu3@gmail.com>
2014-01-01 18:29:41 -0500

    Update installer1.sh
    fixed line 43

--- installer1.sh
+++ installer1.sh
@@ -1,5 +1,4 @@
 
-
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
@@ -40,7 +39,7 @@ echo "# update blacklist each 4 hours
 0 */4 * * * * root /usr/local/bin/voipbl.sh" >> /etc/cron.d/voipbl
 
 echo "#!/bin/bash
-wget -qO - http://www.voipbl.org/update/ | awk '{print \"iptables -A INPUT -source \"\$1\" -j DROP\"}'" >> /usr/local/bin/voipbl.sh
+wget -qO - http://www.voipbl.org/update/ | awk '{print "iptables -A INPUT -s "$1" -j DROP"}' >> /usr/local/bin/voipbl.sh
 
 echo "[asterisk-iptables]
 action   = iptables-allports[name=ASTERISK, protocol=all]
